1d68846f1bac629acccc5a002b85713f155be0d3
dinesh <dinesh.singh14@wipro.com>
2021-07-27 18:37:48 +0530

    initial commit

---
+++ container/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingApp from './modules/MarketingApp';
+
+export default () => {
+    return <div>
+            <h1>Main container</h1>
+            <hr />
+            <MarketingApp />
+        </div>
+}
